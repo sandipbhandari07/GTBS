66dd543d94aef05630788807111bdc21614ba51c
sandipbhandari <bhandarisandip882@gmail.com>
2022-08-01 15:56:10 +0545

    gtbs

--- GTBS/app/src/main/java/clz/project/gtbs/CartAct.java
+++ GTBS/app/src/main/java/clz/project/gtbs/CartAct.java
@@ -1,10 +1,13 @@
 package clz.project.gtbs;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,9 +16,12 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -62,6 +68,52 @@ public class CartAct extends AppCompatActivity {
                         holder.txtproductquantity.setText("Quantity = "+model.getQuantity());
                         holder.txtproductprice.setText("Price ="+model.getPrice()+ "");
                         holder.txtproductname.setText("Prodcutname = "+model.getPname());
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                CharSequence options[] = new CharSequence[]
+                                        {
+                                          "Edit",
+                                          "Remove"
+                                        };
+                                AlertDialog.Builder builder=new AlertDialog.Builder(CartAct.this);
+                                builder.setTitle("Cart Options:");
+
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int i) {
+                                        if (i==0)
+                                        {
+                                            Intent intent=new Intent(CartAct.this,ProductDetails.class);
+                                            intent.putExtra("pid",model.getPid());
+                                            startActivity(intent);
+                                        }
+                                        if (i==1)
+                                        {
+                                            cartListref.child("User View")
+                                                    .child(feature.currentonlineUser.getPhone()).child("Products")
+                                                    .child(model.getPid())
+                                                    .removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if (task.isSuccessful())
+                                                            {
+                                                                Toast.makeText(CartAct.this, "Item removed", Toast.LENGTH_SHORT).show();
+                                                                Intent intent=new Intent(CartAct.this,Homepage.class);
+                                                                startActivity(intent);
+
+                                                            }
+                                                        }
+                                                    });
+                                        }
+
+
+                                    }
+                                });
+                                builder.show();
+                            }
+                        });
 
                     }
 
